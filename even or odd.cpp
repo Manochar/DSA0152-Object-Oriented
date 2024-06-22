d8279c78e9c317c6997ab5e83f6bcdb0f975378b
Manochar <132635065+Manochar@users.noreply.github.com>
2024-06-22 08:58:33 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the numbers:";
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<" is even number"<<std::endl;
+		
+	}
+	else
+	{
+		cout<<"is odd number"<<std::endl;
+	}
+	return 0;
+}
